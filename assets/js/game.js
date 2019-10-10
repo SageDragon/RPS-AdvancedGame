b7452d710b111aa6373c0fda0c2c44541ad58784
Sage Dragon <sagethunderdragon@gmail.com>
2019-10-10 12:54:21 -0400

    Global vars for the app.

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,27 +7,51 @@ var firebaseConfig = {
     messagingSenderId: "184158440206",
     appId: "1:184158440206:web:01e13693a0cf5b947ce55f",
     measurementId: "G-PSS96JSBD9"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var db = firebase.database();
+var db = firebase.database();
+var playersRef = db.ref("/players");
+var chatRef = db.ref("/chat");
+var connectedRef = db.ref("/connected");
+var clicktest = 0;
+var player1login = false;
+var player2login = false;
+var firstPlayer = {
+    name: "",
+    choice: "",
+    wins: 0,
+    losses: 0,
+};
+var secondPlayer = {
+    name: "",
+    choice: "",
+    wins: 0,
+    losses: 0,
+};
 
-  var clicktest = 0;
 
-  let playerName =$("#playerName").val()
+let playerName =$("#playerName").val()
+let chatMessage =$("#chat").val()
 
-  $("#click-button").on("click", function(){
 
+$("#login").on("click", function(event){
+    event.preventdefault();
     clicktest++;
-
     db.ref().set({
         clickcounter: clicktest
     });
     console.log(clicktest)
     console.log(playerName)
-  });
+});
 
-  $("#send-button").on("click", function(){
-    
-  });
+$("#send-button").on("click", function(event){
+    event.preventdefault();
+    clicktest++;
+    db.ref().set({
+        clickcounter: clicktest
+    });
+    console.log(clicktest)
+    console.log(playerName)
+});
